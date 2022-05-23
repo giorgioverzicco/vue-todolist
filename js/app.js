cf2836c99e4bf466401650d567167d7f13e4794e
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-23 14:33:47 +0200

    add main logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,56 @@
+let id = 0;
+
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    newTodo: "",
+    todos: [
+      {
+        id: id++,
+        text: "Fare il meeting con il team",
+        done: false,
+        editMode: false,
+      },
+      {
+        id: id++,
+        text: "Chiamare mamma",
+        done: false,
+        editMode: false,
+      },
+      {
+        id: id++,
+        text: "Fare la spesa",
+        done: false,
+        editMode: false,
+      },
+      {
+        id: id++,
+        text: "Partecipare alla lezione",
+        done: true,
+        editMode: false,
+      },
+    ],
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) return;
+
+      this.todos.push({
+        id: id++,
+        text: this.newTodo.trim(),
+        done: false,
+        editMode: false,
+      });
+      this.newTodo = "";
+    },
+    completeTodo(todo) {
+      todo.done = !todo.done;
+    },
+    editTodo(todo) {
+      todo.editMode = !todo.editMode;
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
 });
